1a9897408b699fae9f45c7331d8a2ff85c458e62
smazepaa <smazepa@kse.ord.ua>
2023-02-12 18:02:23 +0200

    added some coments

--- assignment_calc/Program.cs
+++ assignment_calc/Program.cs
@@ -1,32 +1,32 @@
 ﻿var variable = "2 + 2 * 2";
 char[] operators = new char[] { '+', '-', '/', '*' };
 
-List<string> operations = new List<string>();
+List<string> operations = new List<string>(); // list of strings (operators)
 
-var buff = "";
-char? oper = null;
+var buff = ""; // creating empty buffer
+char? oper = null; // for storing the operator
 
 foreach (var ch in variable)
 {
-    if (Char.IsDigit(ch))
+    if (Char.IsDigit(ch)) //checking if it's a digit
     {
-        buff += ch;
+        buff += ch; // if yes - adding it to the buffer
     }
-    else if (operators.Contains(ch))
+    else if (operators.Contains(ch)) // checking if it's an operator
     {
-        operations.Add(buff);
-        buff = "";
-        if (oper is not null)
+        operations.Add(buff); // adding this operator to the buffer
+        buff = ""; // cleaning the buff
+        if (oper is not null) // checking if oper is not empty
         {
-            operations.Add(oper.ToString());
+            operations.Add(oper.ToString()); //if not - addind a string of that operator
         }
-        oper = ch;
+        oper = ch; 
     }
 }
 
-if (buff != "")
+if (buff != "") // if buff contains something
 {
-    operations.Add(buff);
+    operations.Add(buff); // add it to the operations
     operations.Add(oper.ToString());
 }
 
